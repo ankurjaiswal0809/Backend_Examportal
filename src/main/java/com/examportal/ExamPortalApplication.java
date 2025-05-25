9c62ea188e632255794d2259c7c76e8e0ec9deea
ankurjaiswal0809 <118099779+ankurjaiswal0809@users.noreply.github.com>
2025-05-25 14:16:20 +0530

    Add files via upload

---
+++ src/main/java/com/examportal/ExamPortalApplication.java
@@ -0,0 +1,14 @@
+package com.examportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExamPortalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExamPortalApplication.class, args);
+		System.out.println("This is test");
+	}
+
+}
